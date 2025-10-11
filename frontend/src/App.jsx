43000dea9cb85e2dd2dfd1d8d26513244fa0c1af
HYELIM KIM <hl0121@naver.com>
2025-10-11 11:15:24 +0900

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
-import LoginPage from "./pages/LoginPage";
+import LoginPage from "./pages/LoginPage.jsx";
 
 function App() {
   return (
